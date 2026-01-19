dee11b62841422ba2604f2e05eccfe8568583348
Aditya-Kumar25 <adityaashu.dynmo@gmail.com>
2026-01-19 20:45:12 +0530

    feat: initialize turborepo with nextjs, prisma, postgres

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,11 @@
+// import {PrismaClient} from"@repo/db/client";
+
+// const prisma = new PrismaClient(); 
+
+export default function Page() {
+  return (
+    <div className="bg-red-500 text-white p-10">
+      TAILWIND TEST
+    </div>
+  );
+}
